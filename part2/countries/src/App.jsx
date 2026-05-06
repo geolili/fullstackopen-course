1e74bef54d32f1c99f5c1689953451dff19a0007
geolili <flammedambre@gmail.com>
2026-05-06 14:17:57 +0200

    exercise 2.18 countries

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [value, setValue] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    if (value.trim() !== "") {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+        .then(response => {
+          const data = response.data
+          const filtered = data.filter((country) =>
+            country.name.common
+            .toLowerCase()
+            .startsWith(value.toLocaleLowerCase())
+          )
+          setCountries(filtered)
+        })
+    } else {
+      setCountries([])
+    }
+  }, [value])
+
+  return (
+    <div>
+      <form>
+        find countries: <input value={value} onChange={(e) => setValue(e.target.value)} />
+      </form>
+      {countries.length > 10 && <p>Too many matches, specify another filter</p>}
+      {countries.length <= 10 && countries.length > 1 && <pre>
+        {countries.map((country) => (
+          <p key={country.cca3}>{country.name.common}</p>
+        ))}
+      </pre>}
+      {countries.length === 1 && <pre>
+        {countries.map((country) => (
+          <div key={country.cca3}>
+            <h1 >{country.name.common}</h1>
+            <p>Capital {country.capital}</p>
+            <p>Area {country.area}</p>
+            <h1>Languages</h1>
+            <ul>
+              {Object.entries(country.languages).map(([code, language]) => (
+                <li key={code}>{language}</li>
+              ))}
+            </ul>
+            <img src={country.flags.png} alt={country.flags.alt} />
+          </div>
+        ))}
+        </pre>}
+    </div>
+  )
+}
+
+export default App
